b7376793dcb5b7c819318b3132d76ec75afbf17b
Henok Kirubel <henokhm2@gmail.com>
2023-07-12 19:58:47 +0300

    Add Poppins font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/reset.module.css';
+import './styles/global.module.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
