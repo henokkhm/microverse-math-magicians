8e7018e4a741fe5c1c1208230d9c2d0bc4b2dd91
Henok Kirubel <henokhm2@gmail.com>
2023-07-14 18:45:36 +0300

    Fix infinite rendering loop using useMemo

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,19 @@
+import { useMemo } from 'react';
 import useFetch from '../hooks/useFetch';
 
 const API_KEY = process.env.REACT_APP_API_NINJAS_API_KEY;
 const QUOTES_URL = 'https://api.api-ninjas.com/v1/quotes';
 
 function Quote() {
-  const { isLoading, data, error } = useFetch(QUOTES_URL, {
-    headers: { 'X-Api-Key': API_KEY },
-    contentType: 'application/json',
-  });
+  const options = useMemo(
+    () => ({
+      headers: { 'X-Api-Key': API_KEY },
+      contentType: 'application/json',
+    }),
+    [],
+  );
+
+  const { isLoading, data, error } = useFetch(QUOTES_URL, options);
 
   if (isLoading) {
     return <p>Loading...</p>;
