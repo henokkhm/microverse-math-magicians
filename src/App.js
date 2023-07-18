1e1751eaec37053149e52019fca2ad97665bfc52
Henok Kirubel <henokhm2@gmail.com>
2023-07-18 21:17:24 +0300

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import styles from './styles/App/App.module.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <main className={styles.app}>
-      <h1 className={styles.pageHeader}>Math Magicians</h1>
-      <div className={styles.content}>
-        <Calculator />
-        <Quote />
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <main className={styles.app}>
+        <h1 className={styles.pageHeader}>Math Magicians</h1>
+        <div className={styles.content}>
+          <Calculator />
+          <Quote />
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <header>
+      <div>Math Magicians</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/calculator">Calculator</a>
+          </li>
+          <li>
+            <a href="/quote">Quote</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
