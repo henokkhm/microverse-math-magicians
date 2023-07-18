afedc6d0893208515a219f54b0abba653f5bca63
Henok Kirubel <henokhm2@gmail.com>
2023-07-18 23:03:49 +0300

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+function Home() {
+  return (
+    <section>
+      <h2>Welcome to our page</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ullam
+        atque! Ad dicta velit et repudiandae eligendi sint maiores minus aliquid
+        porro perspiciatis magnam animi assumenda illum neque eos possimus vitae
+        similique maxime, nostrum natus. Eaque minima voluptas voluptatum
+        possimus necessitatibus laborum quibusdam voluptatibus maiores ipsum
+        deleniti sit ratione non dolore, natus reprehenderit voluptates rem
+        molestias quidem ab enim eos officia atque soluta assumenda.
+      </p>
+      <p>
+        Vel, aut error cumque veritatis esse quos facilis suscipit neque in!
+        Cumque modi doloribus repellat ipsa, totam odio adipisci error
+        doloremque nihil esse quos maiores vel quo a est libero deleniti quod
+        omnis culpa labore quam? Tempore quaerat distinctio repudiandae
+        reprehenderit corporis exercitationem ratione sit, aliquam excepturi
+        nostrum laborum a ex optio inventore eum unde nulla, molestias rerum est
+        non debitis praesentium soluta aspernatur. Asperiores, laborum.
+      </p>
+    </section>
+  );
+}
+
+export default Home;
