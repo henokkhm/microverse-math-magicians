07fc41d0e1fc5c709d912a60185c5cbbd7453f0b
Henok Kirubel <henokhm2@gmail.com>
2023-07-18 23:17:24 +0300

    Define Routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
+
 import styles from './styles/App/App.module.css';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
@@ -10,9 +12,11 @@ function App() {
       <Navbar />
       <main className={styles.app}>
         <div className={styles.content}>
-          <Home />
-          <Calculator />
-          <Quote />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quote" element={<Quote />} />
+          </Routes>
         </div>
       </main>
     </>
