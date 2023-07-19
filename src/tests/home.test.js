6a842ffa691d4a6be8897fe5ddf94f56947a45cf
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-19 09:00:54 -0400

    Add test

--- src/tests/home.test.js
+++ src/tests/home.test.js
@@ -1,7 +1,7 @@
 import renderer from 'react-test-renderer';
 import Home from '../components/Home';
 
-it('renders correctly', () => {
+it('The homepage renders correctly', () => {
   const tree = renderer.create(<Home />).toJSON();
   expect(tree).toMatchSnapshot();
 });
