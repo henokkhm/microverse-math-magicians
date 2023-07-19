fe487c08bdb16fce5cc82443bf6436a636d32862
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-19 09:31:11 -0400

    Add result bar testing

---
+++ src/tests/resultBar.test.js
@@ -0,0 +1,19 @@
+import renderer from 'react-test-renderer';
+import ResultBar from '../components/ResultBar';
+
+describe('The resultBar', () => {
+  it('renders 0 if we don\'t pass a display value', () => {
+    const tree = renderer.create(<ResultBar />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the value passed', () => {
+    const tree = renderer.create(<ResultBar displayValue={5} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the value passed', () => {
+    const tree = renderer.create(<ResultBar displayValue={9} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
