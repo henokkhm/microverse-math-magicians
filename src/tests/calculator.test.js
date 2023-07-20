dde50678591ce8fda03a2bb65ae75df4fac2c019
Henok Kirubel <henokhm2@gmail.com>
2023-07-20 15:43:49 +0300

    Add calculator user interaction tests

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,7 +1,42 @@
 import renderer from 'react-test-renderer';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import Calculator from '../components/Calculator';
 
-it('The calculator renders correctly', () => {
-  const tree = renderer.create(<Calculator />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('The calculator component', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('adds one and two correctly', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const one = await screen.findByTestId('btn-1');
+    const plus = await screen.findByTestId('btn-+');
+    const two = await screen.findByTestId('btn-2');
+    const equals = await screen.findByTestId('btn-=');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(one);
+    });
+
+    await act(async () => {
+      await userEvent.click(plus);
+    });
+
+    await act(async () => {
+      await userEvent.click(two);
+    });
+
+    await act(async () => {
+      await userEvent.click(equals);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('3');
+  });
 });
