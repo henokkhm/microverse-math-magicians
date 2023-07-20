b0b5e02b148a6e9f6493816d2cb4225101b81f31
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-20 09:02:01 -0400

    Add more testing

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -39,4 +39,94 @@ describe('The calculator component', () => {
     const calculationResult = await screen.findByTestId('calculation-result');
     expect(calculationResult).toHaveTextContent('3');
   });
+
+  test('multiply one and two correctly', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const one = await screen.findByTestId('btn-1');
+    const multiply = await screen.findByTestId('btn-x');
+    const two = await screen.findByTestId('btn-2');
+    const equals = await screen.findByTestId('btn-=');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(one);
+    });
+
+    await act(async () => {
+      await userEvent.click(multiply);
+    });
+
+    await act(async () => {
+      await userEvent.click(two);
+    });
+
+    await act(async () => {
+      await userEvent.click(equals);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('2');
+  });
+
+  test('subtract one from two correctly', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const one = await screen.findByTestId('btn-1');
+    const subtract = await screen.findByTestId('btn--');
+    const two = await screen.findByTestId('btn-2');
+    const equals = await screen.findByTestId('btn-=');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(two);
+    });
+
+    await act(async () => {
+      await userEvent.click(subtract);
+    });
+
+    await act(async () => {
+      await userEvent.click(one);
+    });
+
+    await act(async () => {
+      await userEvent.click(equals);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('1');
+  });
+
+  test('divide nine by three correctly', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const nine = await screen.findByTestId('btn-9');
+    const divide = await screen.findByTestId('btn-÷');
+    const three = await screen.findByTestId('btn-3');
+    const equals = await screen.findByTestId('btn-=');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(nine);
+    });
+
+    await act(async () => {
+      await userEvent.click(divide);
+    });
+
+    await act(async () => {
+      await userEvent.click(three);
+    });
+
+    await act(async () => {
+      await userEvent.click(equals);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('3');
+  });
 });
