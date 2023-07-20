1575d992035bb906227ca8a8ceb5329e111d5614
Henok Kirubel <henokhm2@gmail.com>
2023-07-20 19:21:50 +0300

    Add tests for decimal point, modulo, +/- and AC buttons

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -129,4 +129,123 @@ describe('The calculator component', () => {
     const calculationResult = await screen.findByTestId('calculation-result');
     expect(calculationResult).toHaveTextContent('3');
   });
+
+  test('operatins involving decimal point work as expected', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const nine = await screen.findByTestId('btn-9');
+    const divide = await screen.findByTestId('btn-÷');
+    const zero = await screen.findByTestId('btn-0');
+    const point = await screen.findByTestId('btn-.');
+    const equals = await screen.findByTestId('btn-=');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(nine);
+    });
+
+    await act(async () => {
+      await userEvent.click(divide);
+    });
+
+    await act(async () => {
+      await userEvent.click(zero);
+    });
+
+    await act(async () => {
+      await userEvent.click(point);
+    });
+
+    await act(async () => {
+      await userEvent.click(nine);
+    });
+
+    await act(async () => {
+      await userEvent.click(equals);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('10');
+  });
+
+  test('modulo operator works as expected', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const nine = await screen.findByTestId('btn-9');
+    const modulo = await screen.findByTestId('btn-%');
+    const two = await screen.findByTestId('btn-2');
+    const equals = await screen.findByTestId('btn-=');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(nine);
+    });
+
+    await act(async () => {
+      await userEvent.click(modulo);
+    });
+
+    await act(async () => {
+      await userEvent.click(two);
+    });
+
+    await act(async () => {
+      await userEvent.click(equals);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('1');
+  });
+
+  test('negative operator works as expected', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const nine = await screen.findByTestId('btn-9');
+    const negative = await screen.findByTestId('btn-+/-');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(nine);
+    });
+
+    await act(async () => {
+      await userEvent.click(negative);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('-9');
+  });
+
+  test('AC button resets result to 0', async () => {
+    // ARRANGE
+    render(<Calculator />);
+    const nine = await screen.findByTestId('btn-9');
+    const plus = await screen.findByTestId('btn-+');
+    const three = await screen.findByTestId('btn-3');
+    const AC = await screen.findByTestId('btn-AC');
+
+    // ACT
+    await act(async () => {
+      await userEvent.click(nine);
+    });
+
+    await act(async () => {
+      await userEvent.click(plus);
+    });
+
+    await act(async () => {
+      await userEvent.click(three);
+    });
+
+    await act(async () => {
+      await userEvent.click(AC);
+    });
+
+    // ASSERT
+    const calculationResult = await screen.findByTestId('calculation-result');
+    expect(calculationResult).toHaveTextContent('0');
+  });
 });
