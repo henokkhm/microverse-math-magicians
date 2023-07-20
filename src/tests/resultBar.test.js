1d8b0fbdab79e54c2f344ec7b2870ece26c632da
Henok Kirubel <henokhm2@gmail.com>
2023-07-20 15:43:24 +0300

    Fix bug in test - resultbar prop must be a string

--- src/tests/resultBar.test.js
+++ src/tests/resultBar.test.js
@@ -2,18 +2,18 @@ import renderer from 'react-test-renderer';
 import ResultBar from '../components/ResultBar';
 
 describe('The resultBar', () => {
-  it('renders 0 if we don\'t pass a display value', () => {
+  it("renders 0 if we don't pass a display value", () => {
     const tree = renderer.create(<ResultBar />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders the value passed', () => {
-    const tree = renderer.create(<ResultBar displayValue={5} />).toJSON();
+    const tree = renderer.create(<ResultBar displayValue="5" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders the value passed', () => {
-    const tree = renderer.create(<ResultBar displayValue={9} />).toJSON();
+    const tree = renderer.create(<ResultBar displayValue="9" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
